c045d6efa0a2f0a6d1e3dd2a783f4bb244736947
Kevin <kevinmanueldiazluis@gmail.com>
2025-04-01 09:28:27 -0700

    Prueba

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  git config --global user.email "kevinmanueldiazluis@gmail.com"
+   git config --global user.name "kevin"
+
+
+*/
